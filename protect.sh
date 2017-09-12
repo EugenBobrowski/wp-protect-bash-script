0ad71aa67a47e0cf40d44aa14e1e24d40c84bf86
Eugen Bobrowski <ecologistkai@gmail.com>
2017-09-12 17:50:58 +0300

    remover readme.html and call another sh

--- protect.sh
+++ protect.sh
@@ -4,17 +4,20 @@ WP_ROOT=$1
 
 pgrep exim | xargs kill -9
 
+if [ -f $WP_ROOT'/readme.html' ]; then
+    rm $WP_ROOT'/readme.html'
+    echo '-------------------------------
+Readme.html was deleted.
+------------------------------'
+fi
+
 if [ ! -f fix-permission.sh ]; then
     wget http://check.dev3.qix.sx/fix-permission.sh
 fi
 
-sh fix-permission.sh $WP_ROOT
+sh ./fix-permission.sh $WP_ROOT
 
-if [ ! -f check.sh ]; then
-    wget http://check.dev3.qix.sx/check.sh
-fi
-
-if [ ! -f .git ]; then
+if [ ! -d .git/ ]; then
     git init
     echo  $WP_ROOT'wp-content/uploads' > .gitignore
     if [ ! -f .gitignore ]; then
@@ -28,5 +31,9 @@ if [ ! -f .git ]; then
     git commit -m 'init' .
 fi
 
+if [ ! -f check.sh ]; then
+    wget http://check.dev3.qix.sx/check.sh
+fi
+
 cd $WP_ROOT
-sh check.sh
+sh ../check.sh
