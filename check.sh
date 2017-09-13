558f1835198a3fb9463feccda86271376a5be169
Eugen Bobrowski <ecologistkai@gmail.com>
2017-09-13 11:54:24 +0300

    check wp only if wp_root specified

--- check.sh
+++ check.sh
@@ -1,62 +1,85 @@
 #!/usr/bin/env bash
 
-echo "============================================
-find ./* -name 'favicon_*'
-Res: find ./* -name 'favicon_*' -delete
---------------------------------------------"
-find ./* -name 'favicon_*'
-echo "\n============================================\n\n"
+WP_ROOT=$1
 
+if [ -z "$WP_ROOT" -a "$WP_ROOT"!=" " ]; then
+    echo "WP_ROOT is missing"
+    exit 2
+fi
 
-echo "============================================
-find ./* -name '*.suspected'
-Res: find ./* -name '*.suspected' -delete
---------------------------------------------"
-find ./* -name '*.suspected'
-echo "\n============================================\n\n"
-
+if [ ! -f ${WP_ROOT}/wp-config.php ]; then
+    echo "File wp-config.php not exists.\nCurrent directory is not a wp root di"
+    exit 2
+fi
 
 echo "============================================
-grep -R '@include \"' ./*
+find $WP_ROOT/* -name 'favicon_*'
+Res: find $WP_ROOT/* -name 'favicon_*' -delete
 --------------------------------------------"
-grep -R '@include "' ./*
-echo "\n============================================\n\n"
+find ${WP_ROOT}/* -name 'favicon_*'
+echo "\n============================================\n"
 
 
 echo "============================================
-grep -R 'strrev' --include=*.php ./*
+find $WP_ROOT/* -name '*.suspected'
+Res: find $WP_ROOT/* -name '*.suspected' -delete
 --------------------------------------------"
-grep -R 'strrev' --include=*.php ./*
-echo "\n============================================\n\n"
+find ${WP_ROOT}/* -name '*.suspected'
+echo "\n============================================\n"
 
 
 echo "============================================
-grep -R 'exit()' --include='*.php' ./* -n | grep '.*:1:' -o | grep '.*.php' -o
+grep -R '@include \"' $WP_ROOT/*
 --------------------------------------------"
-grep -R 'exit()' --include='*.php' ./* -n | grep '.*:1:' -o | grep '.*.php' -o
-echo "\n============================================\n\n"
+grep -R '@include "' ${WP_ROOT}/*
+echo "\n============================================\n"
 
 
 echo "============================================
-grep -R '@serialize' ./*
+grep -R 'strrev' --include=*.php $WP_ROOT/*
 --------------------------------------------"
-grep -R '@serialize' ./*
-echo "\n============================================\n\n"
+grep -R 'strrev' --include=*.php ${WP_ROOT}/*
+echo "\n============================================\n"
 
 
 echo "============================================
-grep -R '\$alphabet' ./*
+grep -R 'exit()' --include='*.php' $WP_ROOT/* -n | grep '.*:1:' -o | grep '.*.php' -o
 --------------------------------------------"
-grep -R '$alphabet' ./*
-echo "\n============================================\n\n"
+grep -R 'exit()' --include='*.php' ${WP_ROOT}/* -n | grep '.*:1:' -o | grep '.*.php' -o
+echo "\n============================================\n"
 
 
 echo "============================================
-grep -R '@\$GLOBALS' --include='*.php' ./*
-grep -R '\$GLOBALS\[\$GLOBALS' --include='*.php' ./*
+grep -R '@serialize' $WP_ROOT/*
 --------------------------------------------"
-grep -R '@$GLOBALS' --include='*.php' ./*
-#grep -R '$GLOBALS' --include='*.php' ./*
-grep -R '$GLOBALS\[$GLOBALS' --include='*.php' ./*
-echo "\n============================================\n\n"
+grep -R '@serialize' ${WP_ROOT}/*
+echo "\n============================================\n"
+
+
+echo "============================================
+grep -R '\$alphabet' $WP_ROOT/*
+--------------------------------------------"
+grep -R '$alphabet' ${WP_ROOT}/*
+echo "\n============================================\n"
+
+
+echo "============================================
+grep -R '@\$GLOBALS' --include='*.php' $WP_ROOT*
+grep -R '\$GLOBALS\[\$GLOBALS' --include='*.php' $WP_ROOT*
+--------------------------------------------"
+grep -R '@$GLOBALS' --include='*.php' ${WP_ROOT}/*
+#grep -R '$GLOBALS' --include='*.php' ${WP_ROOT}/*
+grep -R '$GLOBALS\[$GLOBALS' --include='*.php' ${WP_ROOT}/*
+echo "\n============================================\n"
+
+
+echo "============================================
+find $WP_ROOT/wp-content/uploads/* -name '*.php'
+Res: find $WP_ROOT/wp-content/uploads/* -name '*.php' -delete
+find $WP_ROOT/wp-content/updraft/* -name '*.php'
+Res: find $WP_ROOT/wp-content/updraft/* -name '*.php' -delete
+--------------------------------------------"
+find ${WP_ROOT}/wp-content/uploads/* -name '*.php'
+find ${WP_ROOT}/wp-content/updraft/* -name '*.php'
+echo "\n============================================\n"
 
