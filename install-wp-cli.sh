3a81977a4fd5d324d2398ae49624df125fe0c3bb
Eugen Bobrowski <ecologistkai@gmail.com>
2017-09-13 13:50:19 +0300

    install php5-cli

--- install-wp-cli.sh
+++ install-wp-cli.sh
@@ -2,7 +2,7 @@
 
 curl -O https://raw.githubusercontent.com/wp-cli/builds/gh-pages/phar/wp-cli.phar
 if [ ! -d /etc/php5 ]; then
-    apt-get install php5-cli
+    apt-get install php5-cli -y
 fi
 php wp-cli.phar --info
 chmod +x wp-cli.phar
